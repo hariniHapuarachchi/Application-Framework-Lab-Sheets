b818a6a07d396d40ec465afcc86266978699c3fa
Hapuarachchi H.D.H.U. it17089296 <it17089296@my.sliit.lk>
2019-04-20 13:25:08 +0530

    add spring boot lab

---
+++ src/main/java/com/example/demo/repository/MessageRepository.java
@@ -0,0 +1,38 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.domain.Message;
+
+public class MessageRepository extends MongoRepository<Message,String>{
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public Message createMessage(@RequestBody Message message) {
+		return null;
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public Message updateMessage(@PathVariable("id") String id,@RequestBody Message message) {
+		return null;
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+	public void deleteMessage(@PathVariable("id") String id) {
+		
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Message> findAllMessages(@RequestBody Message message) {
+		return null;
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public Message findMessage(@PathVariable("id") String id,@RequestBody Message message) {
+		return null;
+	}
+}
